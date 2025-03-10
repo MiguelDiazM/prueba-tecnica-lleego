01353cc178f6cdd2e5fa921b113c964d0aeeaa2d
MiguelDiazM <mdm947531@gmail.com>
2025-03-10 15:41:48 +0100

    Ejercicio 2 comentado

--- PHP/ejercicios.php
+++ PHP/ejercicios.php
@@ -1,11 +1,10 @@
 <?php
 //Ejercicio 1
-
 //TODO: Funcion que al pasarle una cadena por parametro devuelve esta cadena pero con las letras de cada palabra al reves
 //@param string $sentence Parametro que contiene la cadena del usuario para aplicarle la función
 //@return string Cadena al reves
 function reverseLettersInWords($sentence){
-    if($sentence==""){
+    if($sentence===""){
         return "Debes introducir algo";
     } else {
         //Separo las palabras de la frase usando el explode, convirtiendolas en arrays independientes
@@ -21,13 +20,15 @@ function reverseLettersInWords($sentence){
 }
 
 //Ejercicio 2
-
+//TODO: Funcion que recibe un array de enteros y devuelve el número mas grande
+//@param Array $numbers Array sobre el que se va a aplicar la función
+//@return int Devuelve el número mas grande del array
 function findLargestNumber($numbers) {
     if(count($numbers)===0){
         return "El array no puede estar vacio";
     } else {
-        $mayor=0;
-        for($i=0;$i<count($numbers);$i++){
+        $mayor=$numbers[0];
+        for($i=1;$i<count($numbers);$i++){
             if($numbers[$i]>$mayor){
                 $mayor = $numbers[$i];
             }
@@ -36,6 +37,7 @@ function findLargestNumber($numbers) {
         return $mayor;
     }
 }
+
 
 
 
