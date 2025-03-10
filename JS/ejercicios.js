c83265d3507964755f7db04d693da4a878671e11
MiguelDiazM <mdm947531@gmail.com>
2025-03-10 21:05:39 +0100

    Ejercicio 1 comentado

--- JS/ejercicios.js
+++ JS/ejercicios.js
@@ -1,8 +1,14 @@
+//Ejercicio 1
+//TODO: Función que con un array de enteros devuelve un array que contiene los números pares
+//@param Array numbers Array que contiene los números para filtrar
+//@return Array que contiene los números pares
 function arrayPairFilter(numbers){
     let pairNumbers = [];
 
+    //Recorro todo el bucle y voy comparando con el operador modulo para comprobar si el número es par
     for(let i=0; i<numbers.length;i++){
         if(numbers[i]%2===0){
+            //Si es par mete ese número en el array de salida
             pairNumbers.push(numbers[i]);
         }
     }
